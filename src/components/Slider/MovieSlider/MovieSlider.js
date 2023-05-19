d0bdea1b398e65e8ad9bd6cd6e6a8b4ce53feb9c
dinhtuannam <namdinhtuan5@gmail.com>
2023-05-20 00:08:00 +0700

    update watch page

--- src/components/Slider/MovieSlider/MovieSlider.js
+++ src/components/Slider/MovieSlider/MovieSlider.js
@@ -1,30 +1,30 @@
-import { useState , useEffect ,Fragment } from "react";
-import { getAllSimiliarService } from "../../../apiService/MovieService";
+import { useState, useEffect, Fragment } from 'react';
+import { getAllSimiliarService } from '../../../apiService/MovieService';
 import { Swiper, SwiperSlide } from 'swiper/react';
 import SwiperCore, { Navigation, Scrollbar, A11y } from 'swiper';
-import tmpImg from '../../../assets/image/netflix-temp-img.png'
+import tmpImg from '../../../assets/image/netflix-temp-img.png';
 import 'swiper/css';
 import 'swiper/css/navigation';
 import 'swiper/css/pagination';
 import 'swiper/css/scrollbar';
-import { apiImgPath } from "../../../utils/publicPath";
-import Skeleton from 'react-loading-skeleton'
-import 'react-loading-skeleton/dist/skeleton.css'
+import { apiImgPath } from '../../../utils/publicPath';
+import Skeleton from 'react-loading-skeleton';
+import 'react-loading-skeleton/dist/skeleton.css';
 SwiperCore.use([Navigation, Scrollbar, A11y]);
 
-function MovieSlider({data}) {
-    const [movie,setMovie] = useState();
-    useEffect(()=>{
-        const getAPIMovie = async() =>{
+function MovieSlider({ data }) {
+    const [movie, setMovie] = useState();
+    useEffect(() => {
+        const getAPIMovie = async () => {
             const response = await getAllSimiliarService(data);
             setMovie(response);
-        }
+        };
         getAPIMovie();
-    },[data])
-     
+    }, [data]);
+
     console.log(movie);
 
-    const RenderMovie = () =>{
+    const RenderMovie = () => {
         return (
             <Swiper
                 breakpoints={{
@@ -34,11 +34,11 @@ function MovieSlider({data}) {
                     },
                     500: {
                         slidesPerView: 4,
-                        spaceBetween: 20
+                        spaceBetween: 20,
                     },
                     700: {
                         slidesPerView: 4,
-                        spaceBetween: 20
+                        spaceBetween: 20,
                     },
                 }}
                 spaceBetween={50}
@@ -50,19 +50,19 @@ function MovieSlider({data}) {
             >
                 {movie.map((movie, index) => {
                     return (
-                        <SwiperSlide key={index} >
+                        <SwiperSlide key={index}>
                             <div className="w-[100px] st:w-[120px] md:w-[160px]">
-                                <a href={`http://localhost:3000/movie/id=${movie.id}`} 
-                                className="inline-block h-[150px]  w-full st:h-[170px] md:h-[210px]">
-                                    {
+                                <a
+                                    href={`/movie/id=${movie.id}`}
+                                    className="inline-block h-[150px]  w-full st:h-[170px] md:h-[210px]"
+                                >
+                                    {(
                                         <img
-                                        src={movie.poster_path?apiImgPath + movie.poster_path:tmpImg}
-                                        className="w-full h-full rounded-2xl"
-                                        alt="movie poster"
+                                            src={movie.poster_path ? apiImgPath + movie.poster_path : tmpImg}
+                                            className="w-full h-full rounded-2xl"
+                                            alt="movie poster"
                                         />
-                                        ||
-                                        <Skeleton />
-                                    }
+                                    ) || <Skeleton />}
                                 </a>
                                 <p className=" text-white opacity-60 mb-4 text-center text-[14px]">{movie.title}</p>
                             </div>
@@ -70,14 +70,10 @@ function MovieSlider({data}) {
                     );
                 })}
             </Swiper>
-        )
-    }
+        );
+    };
 
-    return ( 
-        <div>
-            {movie?RenderMovie():<Fragment></Fragment>}
-        </div>
-    );
+    return <div>{movie ? RenderMovie() : <Fragment></Fragment>}</div>;
 }
 
 export default MovieSlider;
